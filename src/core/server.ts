9edd163c7256cfda900d0a5ddbd19661b755246f
Miguel Gomez <mgomez.developer@gmail.com>
2021-03-03 12:29:48 -0500

    Setup express and Add nodemon

--- src/core/server.ts
+++ src/core/server.ts
@@ -1,16 +1,29 @@
 import { createServer, IncomingMessage, ServerResponse } from 'http';
 import { parse } from 'url';
 
+import express from 'express';
+
 export class Server {
   public setupNodeServer() {
+    const server = express();
+
     createServer((req: IncomingMessage, res: ServerResponse) => {
+      server.use(express.json());
+
       // http://localdomain/anyUrlpassed
       console.log(`Incoming Request Type: ${req.method}`);
       const baseUrl = Url.getBasePathUrl(req.url);
 
       res.end();
-    }).listen(8080);
-    console.log(`Starting Node server on port 8080`);
+    });
+
+    server.use('/', (req, res) => {
+      res.send('Checking Root Path of Server');
+    });
+
+    server.listen(8080, () => {
+      console.log(`Starting Node server on port 8080`);
+    });
   }
 }
 
